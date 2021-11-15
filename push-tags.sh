cddbc70b205b306c1457069cacbd11dbe392cab0
wanrenzhizun <245402643@qq.com>
2021-11-16 02:31:41 +0800

    custom

--- push-tags.sh
+++ push-tags.sh
@@ -7,11 +7,11 @@ SE_REVISION="9760"
 BASE_TAGS="latest centos debian alpine ubuntu"
 
 for TAG in ${BASE_TAGS}; do
-  docker pull siomiz/softethervpn:${TAG}
+  docker pull wanrenzhizun/softether:${TAG}
   VERSION_TAG=${SE_VERSION}-${TAG}
   REVISION_TAG=${SE_REVISION}-${TAG}
-  docker tag siomiz/softethervpn:${TAG} siomiz/softethervpn:${VERSION_TAG%-latest}
-  docker tag siomiz/softethervpn:${TAG} siomiz/softethervpn:${REVISION_TAG%-latest}
-  docker push siomiz/softethervpn:${VERSION_TAG%-latest}
-  docker push siomiz/softethervpn:${REVISION_TAG%-latest}
+  docker tag wanrenzhizun/softether:${TAG} siomiz/softethervpn:${VERSION_TAG%-latest}
+  docker tag wanrenzhizun/softether:${TAG} siomiz/softethervpn:${REVISION_TAG%-latest}
+  docker push wanrenzhizun/softether:${VERSION_TAG%-latest}
+  docker push wanrenzhizun/softether:${REVISION_TAG%-latest}
 done
